2e3106cb6bb72f614e36f2aea1620daf373e78ee
Nolan Byrnes <nolanehs@gmail.com>
2018-02-28 09:39:05 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,20 @@
 class SongsController < ApplicationController
 
+  def create
+    @song = Song.new(song_params(:title, :released, :release_year, :artist_name, :genre))
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params(:title, :released, :release_year, :artist_name, :genre))
+    redirect_to song_path(@song)
+  end
+
+
+
+
   def destroy
     Song.find(params[:id]).destroy
     redirect_to song_url
@@ -9,5 +24,5 @@ private
 def song_params(*args)
   params.require(:song).permit(*args)
 end
-  
+
 end
