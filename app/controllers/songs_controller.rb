0969f370e6a708ccc7b1d8bb650fa79ec14440de
Nolan Byrnes <nolanehs@gmail.com>
2018-02-28 09:36:20 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,13 @@
 class SongsController < ApplicationController
+
+  def destroy
+    Song.find(params[:id]).destroy
+    redirect_to song_url
+  end
+
+private
+def song_params(*args)
+  params.require(:song).permit(*args)
+end
+  
 end
