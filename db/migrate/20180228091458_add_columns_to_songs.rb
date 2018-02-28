aea4f1a31c0ae70fcb9283247c834f1fa7428b85
Nolan Byrnes <nolanehs@gmail.com>
2018-02-28 09:18:18 +0000

    automatically backed up by learn

--- db/migrate/20180228091458_add_columns_to_songs.rb
+++ db/migrate/20180228091458_add_columns_to_songs.rb
@@ -1,4 +1,9 @@
 class AddColumnsToSongs < ActiveRecord::Migration
   def change
-  end
+    add_column :songs, :title, :string
+    add_column :songs, :released, :boolean
+    add_column :songs, :release_year, :integer
+    add_column :songs, :artist_name, :string
+    add_column :songs, :genre, :string
+      end
 end
